8b515e88ef9dc5f6bbebe5ce98cd86a2d5f73528
Peter Schmid <peter.schmid@mirai-solutions.com>
2023-01-31 12:24:02 +0100

    Deploying locally worked after adding the account, so this should solve it also on gh actions

--- deploy/deploy-shinyapps.R
+++ deploy/deploy-shinyapps.R
@@ -5,4 +5,7 @@ rsconnect::setAccountInfo(
   Sys.getenv("SHINYAPPS_TOKEN"),
   Sys.getenv("SHINYAPPS_SECRET")
 )
-rsconnect::deployApp(appName = "ShinyCICD")
+rsconnect::deployApp(
+  account = "miraisolutions",
+  appName = "ShinyCICD"
+)
