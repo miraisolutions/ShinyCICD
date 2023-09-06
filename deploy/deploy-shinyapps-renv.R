8ef4e4b18a9dc2a8d7e93ac78d63c6f1d5076157
GuidoMaggio <guido.maggio@mirai-solutions.com>
2023-09-06 13:28:13 +0200

    test CI-CD-renv deployment

---
+++ deploy/deploy-shinyapps-renv.R
@@ -0,0 +1,9 @@
+# deploy/deploy-shinyapps.R
+# usethis::use_build_ignore("deploy")
+rsconnect::setAccountInfo(
+  Sys.getenv("SHINYAPPS_ACCOUNT"),
+  Sys.getenv("SHINYAPPS_TOKEN"),
+  Sys.getenv("SHINYAPPS_SECRET")
+)
+
+rsconnect::deployApp(appName = "ShinyCICD-test", forceUpdate = TRUE)
