57925412970c3fd9e60d4ef86736c4537e960ad8
Riccardo Porreca <riccardo.porreca@mirai-solutions.com>
2024-01-10 13:06:48 +0100

    Fix R CMD CHECK NOTE about `faithful` dataset as global variable
    * Note that `importFrom("datasets", "faithful")`, as hinted in the NOTE, does not seem to work for datasets.

--- R/app_server.R
+++ R/app_server.R
@@ -8,7 +8,7 @@
 app_server <- function(input, output, session) {
   output$distPlot <- renderPlot({
     # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
+    x    <- datasets::faithful[, 2]
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
     # draw the histogram with the specified number of bins
