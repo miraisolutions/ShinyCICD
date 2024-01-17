06446f65987f0dedb2c5040078dc2f39a2533abe
Riccardo Porreca <riccardo.porreca@mirai-solutions.com>
2024-01-17 11:19:04 +0100

    Allow using `deploy-shinyapps.R` locally to rely on the specific `appFiles`
    * This assumes one has configured an account locally via `rsconnect::setAccountInfo()`.

--- deploy/deploy-shinyapps.R
+++ deploy/deploy-shinyapps.R
@@ -1,10 +1,12 @@
 # deploy/deploy-shinyapps.R
 # usethis::use_build_ignore("deploy")
-rsconnect::setAccountInfo(
-  Sys.getenv("SHINYAPPS_ACCOUNT"),
-  Sys.getenv("SHINYAPPS_TOKEN"),
-  Sys.getenv("SHINYAPPS_SECRET")
-)
+if (!interactive()) {
+  rsconnect::setAccountInfo(
+    Sys.getenv("SHINYAPPS_ACCOUNT"),
+    Sys.getenv("SHINYAPPS_TOKEN"),
+    Sys.getenv("SHINYAPPS_SECRET")
+  )
+}
 # Add here any additional files/directories the app needs
 app_files = c(
   "app.R",
